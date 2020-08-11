7e647b6be1d99c9fd7abc678256d9449b0e23a51
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2020-08-10 20:12:15 -0400

    8.5

--- part8/library/backend.js
+++ part8/library/backend.js
@@ -97,7 +97,7 @@ const typeDefs = gql`
   	type Query {
 	  bookCount: Int!
 	  authorCount: Int!
-	  allBooks(author: String): [Book!]!
+	  allBooks(author: String, genre: String): [Book!]!
 	  allAuthors: [Author!]!
   }
 `
@@ -107,8 +107,19 @@ const resolvers = {
 		bookCount: () => books.length,
 		authorCount: () => authors.length,
 		allBooks: (root, args) => {
-			if (!args.author) return books
-			return books.filter(b => b.author === args.author)
+			let filteredBooks = books.map(b => b)
+			if (!(args.author || args.genre)) return books
+			else {
+				if (args.author)
+					filteredBooks = filteredBooks.filter(b => b.author === args.author)
+				if (args.genre) {
+					filteredBooks = filteredBooks.filter(b =>
+						b.genres.find(g => g === args.genre)
+					)
+				}
+
+				return filteredBooks
+			}
 		},
 		allAuthors: () => authors
 	},
