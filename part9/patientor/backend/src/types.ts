cb43bff561d169e65fef69300bd7a3e709b3e342
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2020-08-29 13:37:36 -0400

    9.19

--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -4,9 +4,49 @@ export enum Gender {
 	Other = 'other'
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+export enum HealthCheckRating {
+	"Healthy" = 0,
+	"LowRisk" = 1,
+	"HighRisk" = 2,
+	"CriticalRisk" = 3
 }
+
+interface Discharge {
+	date: string;
+	criteria: string;
+}
+
+interface SickLeave {
+	startDate: string;
+	endDate: string;
+}
+
+interface BaseEntry {
+	id: string;
+	description: string;
+	date: string;
+	specialist: string;
+	diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+	type: "HealthCheck";
+	healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+	type: "Hospital";
+	description: string;
+	discharge: Discharge;
+}
+
+interface OccupationalEntry extends BaseEntry {
+	type: "OccupationalHealthcare";
+	employerName: string;
+	sickLeave?: SickLeave;
+}
+
+export type Entry = HealthCheckEntry | HospitalEntry | OccupationalEntry;
 
 export interface Diagnosis {
 	code: string,
