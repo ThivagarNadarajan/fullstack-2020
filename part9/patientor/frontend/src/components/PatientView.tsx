1365856cff2f154ed6684996e0d58b53c1611b38
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2020-08-29 14:11:47 -0400

    9.20

--- part9/patientor/frontend/src/components/PatientView.tsx
+++ part9/patientor/frontend/src/components/PatientView.tsx
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 import { apiBaseUrl } from "../constants";
 import { useStateValue } from "../state";
 import { Icon, Header } from "semantic-ui-react";
+import { Entry } from '../types';
 
 import { updatePatient } from '../state/reducer';
 
@@ -23,13 +24,30 @@ const PatientView: React.FC = () => {
 		}
 	};
 
+	const handleEntry = (entry: Entry) => {
+		return (
+			<div key={entry.id}>
+				{entry.date} {entry.description}
+				<ul>
+					{entry.diagnosisCodes?.map(code =>
+						<li key={code}>{code}</li>)}
+				</ul>
+			</div>
+		);
+	}
+
 	if (patients[patientId]) {
 		if (patients[patientId].ssn === undefined) findPatient();
 		let patient = patients[patientId];
+
+		let entries: Entry[] = [];
+		if (patient.entries !== undefined) entries = patient.entries;
+
 		let gender: "mars" | "venus" | "neuter";
 		if (patient.gender === "male") gender = "mars";
 		else if (patient.gender === "female") gender = "venus";
 		else gender = "neuter"
+
 		return (
 			<div>
 				<Header as="h2">
@@ -39,7 +57,12 @@ const PatientView: React.FC = () => {
 
 				{`SSN: ${patient.ssn}`}
 				<br />
+				{`Date of Birth: ${patient.dateOfBirth}`}
+				<br />
 				{`Occupation: ${patient.occupation}`}
+				<br />
+				<Header as="h3">Entries</Header>
+				{entries.map(entry => handleEntry(entry))}
 			</div>
 		);
 	}
--- part9/patientor/frontend/src/types.ts
+++ part9/patientor/frontend/src/types.ts
@@ -1,20 +1,65 @@
 export interface Diagnosis {
-  code: string;
-  name: string;
-  latin?: string;
+	code: string;
+	name: string;
+	latin?: string;
 }
 
 export enum Gender {
-  Male = "male",
-  Female = "female",
-  Other = "other"
+	Male = "male",
+	Female = "female",
+	Other = "other"
 }
 
+export enum HealthCheckRating {
+	"Healthy" = 0,
+	"LowRisk" = 1,
+	"HighRisk" = 2,
+	"CriticalRisk" = 3
+}
+
+interface Discharge {
+	date: string;
+	criteria: string;
+}
+
+interface SickLeave {
+	startDate: string;
+	endDate: string;
+}
+
+interface BaseEntry {
+	id: string;
+	description: string;
+	date: string;
+	specialist: string;
+	diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+	type: "HealthCheck";
+	healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+	type: "Hospital";
+	description: string;
+	discharge: Discharge;
+}
+
+interface OccupationalEntry extends BaseEntry {
+	type: "OccupationalHealthcare";
+	employerName: string;
+	sickLeave?: SickLeave;
+}
+
+export type Entry = HealthCheckEntry | HospitalEntry | OccupationalEntry;
+
 export interface Patient {
-  id: string;
-  name: string;
-  occupation: string;
-  gender: Gender;
-  ssn?: string;
-  dateOfBirth?: string;
+	id: string;
+	name: string;
+	occupation: string;
+	gender: Gender;
+	ssn?: string;
+	dateOfBirth?: string;
+	entries: Entry[];
 }
